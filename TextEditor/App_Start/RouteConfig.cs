d94c3ba195c7958a9796c6b756e2c7fc265f1ac7
lehieu1312 <hieulm1312@gmail.com>
2017-04-01 14:16:30 +0700

    sss

--- TextEditor/App_Start/RouteConfig.cs
+++ TextEditor/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace TextEditor
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "TextEditor.Controllers" }
             );
         }
     }
