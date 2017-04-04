5911da3a2e51ec335003f3b94002002d67bdd878
lehieu1312 <hieulm1312@gmail.com>
2017-04-04 20:42:27 +0700

    adđ

---
+++ TextEditor/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TextEditor.Models;
+
+namespace TextEditor.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Users()
+        {
+            var db = new ApplicationDbContext();
+            var result = (from u in db.Users
+                          select new Models.UserViewModel
+                          {
+                              Id=u.Id,
+                              FullName = u.Fullname,
+                              Email=u.Email,
+                              Address =u.Address   
+                          }
+                        );
+
+            return View(result.ToList());
+        }
+        public ActionResult Del(string id)
+        {
+            var db = new ApplicationDbContext();
+            var result = db.Users.Find(id);
+            db.Users.Remove(result);
+            db.SaveChanges();
+
+            return RedirectToAction("Users");
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
