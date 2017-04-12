210c9b2f6ecfbbe89eb7ec713900a6483447ad8c
Hoàng Nguyễn <lgo.hoang@outlook.com>
2017-04-13 01:18:59 +0700

    sssssssssss

--- TextEditor/Models/Compare.cs
+++ TextEditor/Models/Compare.cs
@@ -23,9 +23,70 @@ namespace TextEditor.Models
         public int PageId { get; set; }
         public int Row { get; set; }
         public string Name { get; set; }
-
         public int Size { get; set; }
         public bool Bold { get; set; }
         public bool Italic { get; set; }
+    }
+
+    public class FormatView
+    {
+        public FormatView()
+        {
+            Page = new PageView();
+            Properties = new Dictionary<string, PropertiesView>();
+        }
+        public PageView Page { get; set; }
+        public Dictionary<string,PropertiesView> Properties { get; set; }
+    }
+
+    public class PageView
+    {
+        public PageView()
+        {
+            MarginTop = new Error();
+            MarginBottom = new Error();
+            MarginLeft = new Error();
+            MarginRight = new Error();
+            PaperType = new Error();
+            FontFamily = new Error();
+        }
+        public bool isOk { get; set; }
+        public Error MarginTop { get; set; }
+        public Error MarginBottom { get; set; }
+        public Error MarginLeft { get; set; }
+        public Error MarginRight { get; set; }
+        public Error PaperType { get; set; }
+        public Error FontFamily { get; set; }
+    }
+
+    public class PropertiesView
+    {
+        public PropertiesView()
+        {
+            FontSize = new Error();
+            FontBold = new Error();
+            FontItalic = new Error();
+        }
+        public bool isOk { get; set; }
+        public string Name { get; set; }
+        public Error FontSize { get; set; }
+        public Error FontBold { get; set; }
+        public Error FontItalic { get; set; }
+    }
+
+    public class Error
+    {
+        public bool isError { get; set; }
+        public string FileValue { get; set; }
+        public string DBValue { get; set; }
+    }
+
+    public class Gramar
+    {
+        public Gramar()
+        {
+            Error = new List<string>();
+        }
+        public List<string> Error { get; set; }
     }
 }
